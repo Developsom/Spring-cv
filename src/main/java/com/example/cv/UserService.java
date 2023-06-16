342d4381ebdeee7f757fe97e75ebfe459c0351d3
Developsom <100343391+Developsom@users.noreply.github.com>
2023-06-16 20:49:16 +0200

    Implemented Authentimiauwcation

---
+++ src/main/java/com/example/cv/UserService.java
@@ -0,0 +1,25 @@
+package com.example.cv;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public boolean authenticateUser(String username, String password) {
+        User user = userRepository.findByUsername(username);
+        if (user != null) {
+            return passwordEncoder.matches(password, user.getPassword());
+        }
+        return false;
+    }
+}
