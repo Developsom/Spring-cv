46b2f8f6b1b224e0b06da094da3f1d782e553507
Developsom <100343391+Developsom@users.noreply.github.com>
2023-06-21 13:49:39 +0200

    fixing circular bean

--- src/main/java/com/example/cv/CvApplication.java
+++ src/main/java/com/example/cv/CvApplication.java
@@ -2,12 +2,13 @@ package com.example.cv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.cv.config")
 public class CvApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CvApplication.class, args);
 	}
-
 }
+
