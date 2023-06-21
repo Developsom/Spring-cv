fa6180689ac5bd18853bc528bc1c19e1cb0f6516
Developsom <100343391+Developsom@users.noreply.github.com>
2023-06-21 16:32:09 +0200

    push

--- src/main/java/com/example/cv/CvApplication.java
+++ src/main/java/com/example/cv/CvApplication.java
@@ -2,11 +2,12 @@ package com.example.cv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class CvApplication {
 	public static void main(String[] args) {
+		// Add a breakpoint on the next line
+		System.out.println("Application starting...");
 		SpringApplication.run(CvApplication.class, args);
 	}
 }
