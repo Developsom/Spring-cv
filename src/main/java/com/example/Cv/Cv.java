98136907bba5427667976de288dd2658c924b3f1
Developsom <100343391+Developsom@users.noreply.github.com>
2023-06-29 23:51:24 +0200

    Resolve datatype

--- src/main/java/com/example/Cv/Cv.java
+++ src/main/java/com/example/Cv/Cv.java
@@ -2,11 +2,11 @@ package com.example.Cv;
 
 public class Cv {
     private String name;
-    private String name;
-    private String name;
-    private String name;
-    private String name;
-    private String name;
+    private String address;
+    private int phoneNumber;
+    private String zipcode; //Resolve datatype
+    private String houseNumber;
+    private String houseNumberAdditional;
     private String name;
 
 }
